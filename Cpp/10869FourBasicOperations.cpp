c52e1d239efc6de8654e75a3d85e78fc5f5018cc
YOO JAE HYUN <deep1995@koreatech.ac.kr>
2025-07-13 13:53:59 +0900

    Create 10869FourBasicOperations.cpp

---
+++ Cpp/10869FourBasicOperations.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int A, B;
+    std::cin >> A >> B;           // A와 B 입력 받기
+    std::cout << A + B << std::endl;  // A + B 출력
+    std::cout << A - B << std::endl;
+    std::cout << A * B << std::endl;
+    std::cout << A / B << std::endl;
+    std::cout << A % B << std::endl;
+    return 0;
+}
