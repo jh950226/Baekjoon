10351a9c370cce9d2c77ffd9269a618357d5ecea
YOO JAE HYUN <deep1995@koreatech.ac.kr>
2025-07-13 13:22:12 +0900

    Update 2557HelloWorld.cpp

--- Cpp/2557HelloWorld.cpp
+++ Cpp/2557HelloWorld.cpp
@@ -1,7 +1,12 @@
 // 2557HelloWorld.cpp
-#include <iostream>
+#include <iostream> // std::cin, std::Cout, std:cerr 쓰기 위해서 필요함.
 
 int main() {
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
+
+// std::cout 표준출력, 화면에 출력할때 사용.
+// :: => 범위지정 연산자.
+// std => 표준네임스페이스 standard namespace의 줄임말
+// ::cout => 표준네임스페이스 표준출력스트림객체를 사용하겠다는 것.
